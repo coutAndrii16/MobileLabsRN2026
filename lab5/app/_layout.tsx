f640ad82053364afea8d76d40b7b3c35a6726243
AndriiMaidanovych <ipz242_mav@student.ztu.edu.ua>
2026-05-09 02:21:03 +0300

    reviewed

--- lab5/app/_layout.tsx
+++ lab5/app/_layout.tsx
@@ -1,10 +1,23 @@
-import { AuthProvider } from '@/context/AuthContext';
-import { Stack } from 'expo-router';
+import { AuthProvider, useAuth } from '@/context/AuthContext';
+import { Redirect, Stack } from 'expo-router';
+
+function RootNavigator() {
+    const { isAuthenticated } = useAuth();
+
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(app)" />
+            <Stack.Screen name="+not-found" />
+        </Stack>
+    );
+}
 
 export default function RootLayout() {
     return (
         <AuthProvider>
-            <Stack screenOptions={{ headerShown: false }} />
+            <RootNavigator />
         </AuthProvider>
     );
 }
